6b51cd8bc9cbc21f457d3776f674a9a683ddac6c
Kevin Castel <keviniicastel@gmail.com>
2025-11-01 00:14:40 +0100

    Finition de la fonctionnalité de l'application

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,124 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { exec } from 'child_process';
 
-test('renders learn react link', () => {
+//Config game intergration test
+
+test('change_game_time', ()=> {
+  render(<App/>);
+
+  const value = 10;
+  const inputTime = screen.getByDisplayValue(300);
+
+  fireEvent.change(inputTime, {
+    target:{
+      value
+    }
+  });
+
+  expect(screen.getByDisplayValue(value)).toBeInTheDocument();
+})
+
+test('change_lives', () => {
+  render(<App/>);
+
+  const value = 40;
+
+  const inputLife = screen.getByDisplayValue(30);
+  fireEvent.change(inputLife, {
+    target : {
+      value
+    }
+  });
+
+  expect(screen.getByDisplayValue(value)).toBeInTheDocument();
+
+});
+
+
+// Game Integratating Tests
+test('defaite_plus_de_temps', () => {
+  render(<App/>);
+
+  const value = 0;
+  const inputTime = screen.getByDisplayValue(300);
+
+  fireEvent.change(inputTime, {
+    object :{
+      value
+    }
+  });
+
+  const buttonPlay = screen.getByText("Jouer");
+  fireEvent.click(buttonPlay);
+
+  
+  
+  expect(screen.getByDisplayValue("Navré que la partie dernière n'est pas été gagnée")).toBeInTheDocument();
+  
+})
+
+
+test('défaite_plus_de_vie', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const changeLife = () => {
+    const value = 1;
+
+    const inputLife = screen.getByDisplayValue(30);
+      fireEvent.change(inputLife, {
+        target:{
+          value
+        }
+      })
+  };
+
+  const changeAnswer = () => {
+    const inputValue = screen.getByDisplayValue(0);
+    const value = '2';
+    fireEvent.change(inputValue, {
+      target: {
+        value
+      }
+    });
+  };
+
+
+  changeLife();
+  changeAnswer();
+
+
+  const buttonPlay = screen.getByText("Jouer");
+  fireEvent.click(buttonPlay);
+
+
+
+  const submitAnswer = screen.getByText("Valider");
+  fireEvent.click(submitAnswer);
+
+  expect(screen.getByText("Navré que la partie dernière n'est pas été gagnée")).toBeInTheDocument();
+
+});
+
+
+test('victoire', () => {
+  render(<App />);
+
+  const buttonPlay = screen.getByText("Jouer");
+  fireEvent.click(buttonPlay);
+
+  const inputValue = screen.getByDisplayValue(0);
+  const value = '30';
+  fireEvent.change(inputValue, {
+    target: {
+      value
+    }
+  });
+
+  const submitAnswer = screen.getByText("Valider");
+  fireEvent.click(submitAnswer);
+
+  // expect(screen.findByText('Félicitation, la dernière partie est gagnée!')).toBeInTheDocument();
+  expect(screen.getByText('Félicitation, la dernière partie est gagnée!')).toBeInTheDocument();
+
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { error } from 'console';
+import { error, timeStamp } from 'console';
 import './App.css';
 import "./index.css"
 import { callbackify } from 'util';
-import { ChangeEvent, Dispatch, SetStateAction, SyntheticEvent, useEffect, useState } from 'react';
+import { ChangeEvent, Dispatch, JSX, SetStateAction, SyntheticEvent, useEffect, useState } from 'react';
 import { Input } from 'postcss';
 import { Target } from 'inspector/promises';
 import { setDefaultCACertificates } from 'tls';
@@ -14,40 +14,65 @@ import { setDefaultCACertificates } from 'tls';
 // import {ReactComponent as HeartSvg} from './heart-svgrepo-com.svg';
 
 
+const MAX_SECONDS = 300;
+const DEFAULT_LIVES : number = 30;
 
-const DEFAULT_LIVES = 3;
+const inputStates = Object.freeze({
+  IS_HIGHER : "is_higher",
+  IS_LOWER : "is_lower",
+  IS_EQUAL : "is_equal",
+})
+
+const gameStatements = Object.freeze({
+  IS_WON:"is_won",
+  IS_LOST:"is_lost",
+})
+
+
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [lives, setLives] = useState(DEFAULT_LIVES);
+  const [lives, setLives] = useState(0);
   const [configLives, setConfigLives] = useState(DEFAULT_LIVES);
   const [numToFind, setNumToFind] = useState(0);
-  // const [stillSeconds, setStillSeconds] = useState(0);
+  const [stillSeconds, setStillSeconds] = useState(MAX_SECONDS);
+  const [configStillSeconds, setConfigStillSeconds] = useState(MAX_SECONDS);
   const [lastInputValue, setLastInputValue] = useState(0);
-  const [isInputHigherThanNumToFind, setIsInputHigherThanNumToFind] = useState(false);  
+  const [inputState, setInputState] = useState("");
+  const [lastGameState, setLastGameState] = useState("none");
 
-      
-    // handleTime(setStillSeconds) https://stackoverflow.com/questions/75373182/how-to-create-a-timer-inside-hooks-on-reactjs
-    
-  const startGame = (isPlaying:boolean) => {
-    setIsPlaying(isPlaying);
-      if (isPlaying){
-        setNumToFind(Math.floor(Math.random()*9999));
-      };
-    }
+  useEffect(() => {
+    const interval = setInterval(()=>{
+        setStillSeconds(stillSeconds-1);
+        if (stillSeconds < 2){
+          LosingGame({setIsPlaying}, {setLastGameState});
+        }
+    }, 1000)
+    return () => clearInterval(interval);
+  }, [stillSeconds, setStillSeconds]);
+
+  const startGame = () => {
+    setIsPlaying(true);
+    setNumToFind(Math.floor(Math.random()*9999));
+    setLives(configLives);
+    setStillSeconds(configStillSeconds);
+    console.log("is playing :", isPlaying);
+  }
   
-    //                                   (newLivesValue:number) => {
-  //                                     setLives(newLivesValue);
-  //                                   },
 
-
+  
+  
   return (
     <div className={isPlaying ? "App-playing" : "App-not-playing"}>
       <header>
-        <GetGameInterface setLives={setLives} lives={lives}
+        <GameInterface lives={lives}
                           setConfigLives={setConfigLives} configLives={configLives}
-                          isPlaying={isPlaying} stillSeconds={-1}
-                          startGame={startGame}/>
+                          setConfigGameTime={setConfigStillSeconds} configGameTime={stillSeconds}
+                          isPlaying={isPlaying} gameTime={stillSeconds}
+                          gameState={lastGameState}
+                          startGame={startGame}
+                          setLives={setLives}
+                          />
       </header>
       <div className={isPlaying ? 'body-customized-playing' : "body-customized-not-playing"}>
         <InputGame numToFind={numToFind}
@@ -55,64 +80,78 @@ function App() {
                     setLastInput={setLastInputValue}
                     setLives={setLives}
                     lives={lives}
-                    setIsPlaying={setIsPlaying}/>
+                    setIsPlaying={setIsPlaying}
+                    setInputState={setInputState} 
+                    minimumValueNumInput={10}
+                    maximumValueNumInput={20}
+                    setLastGameState={setLastGameState}/>
         <InputInformationParty
             isPlaying={isPlaying}
             lastInputValue={lastInputValue}
-            isInputHigherThanNumToFind={isInputHigherThanNumToFind}/>
+            inputState={inputState}
+            />
       </div>
       <div className='App-footer'>
-        <p>Je suis footer<br />
-        https://www.svgrepo.com/svg/535436/heart - Heart Icon</p>
+        <p>Produit avec Reactjs<br/>Heberger sur github.com</p>
       </div>
     </div>
   );
 }
 
-//handle the time of the party during the game
-// function handleTime(setStillSeconds:Dispatch<SetStateAction<number>>) : boolean{
-//   useEffect(()=>{
 
-//   })
-//   return true;
-// }
 
-function LosingGame({setIsPlaying}:{setIsPlaying:Dispatch<SetStateAction<boolean>>}){
+function LosingGame({setIsPlaying}:{setIsPlaying:Dispatch<SetStateAction<boolean>>},{setLastGameState}:{setLastGameState:Dispatch<SetStateAction<string>>}){
   setIsPlaying(false);
+  setLastGameState(gameStatements.IS_LOST);
+  
 }
 
 interface GameInterfaceProps{
   isPlaying: boolean,
   lives: number,
-  stillSeconds: number,
-  startGame: (v:boolean) => void,
+  gameTime: number,
+  startGame: () => void,
   setLives:Dispatch<SetStateAction<number>>,
   setConfigLives:Dispatch<SetStateAction<number>>,
+  configGameTime : number,
+  setConfigGameTime : Dispatch<SetStateAction<number>>,
   configLives:number,
+  gameState:string,
 }
 
 //Return the appropriate game menu interface for 
-function GetGameInterface({isPlaying, lives, stillSeconds, startGame,
-                          setLives, setConfigLives, configLives}:GameInterfaceProps){
+function GameInterface({lives, isPlaying, gameTime, startGame,
+                          setConfigLives, configGameTime, setConfigGameTime, configLives,
+                          gameState}:GameInterfaceProps){
   if (isPlaying) {
-    return InformationsGame(lives, stillSeconds);
+    return <InformationsGame liveStill={lives} timeStill={gameTime}/>
   }
-  return ConfigurationGame(startGame, setLives, configLives, setConfigLives);
+  return <ConfigurationGame configLives={configLives} configGameTime={configGameTime}
+                lastGameState={gameState} setConfigLives={setConfigLives}
+                setConfigGameTime={setConfigGameTime} startGame={startGame}/>
 }
 
 
 interface InputInformationPartyProps {
   isPlaying : boolean;
   lastInputValue : number,
-  isInputHigherThanNumToFind : boolean;
+  inputState : string;
 }
 
 //Component for rendering game informations on input user during the game 
-function InputInformationParty({isPlaying, lastInputValue, isInputHigherThanNumToFind}:InputInformationPartyProps){
+function InputInformationParty({isPlaying, lastInputValue, inputState}:InputInformationPartyProps){
   if (!isPlaying){
     return null;
   }
-  const messageOnTheValue = isInputHigherThanNumToFind?"La valeur est trop haute":"La valeur est trop basse";
+  let messageOnTheValue = "";
+  // const messageOnTheValue = isInputHigherThanNumToFind?"La valeur est trop haute":"La valeur est trop basse";
+  if (inputState == inputStates.IS_EQUAL){
+    messageOnTheValue = "Bravo ! C'est gagné !";
+  } else if (inputState == inputStates.IS_LOWER){
+    messageOnTheValue = "La valeur est trop petite";
+  } else if (inputState == inputStates.IS_HIGHER){
+    messageOnTheValue = "La valeur est trop haute";
+  }
 
 
   return (<div className='infos-party'>
@@ -120,15 +159,16 @@ function InputInformationParty({isPlaying, lastInputValue, isInputHigherThanNumT
   </div>)
 }
 
-/* Components for detailling during game */
-function InformationsGame(liveStill:number, timeStill:number){
-  let index = 0;
-  // const images = []
-  const svg_size = "14%";
-  // for(index = 0; index < liveStill; index ++){
-  //   images[index] = <img height={svg_size} width={svg_size} key={`life-${index}`} src={HeartLife} alt={`life-${index}`}/>
-  // }
+interface InformationGameProps {
+  liveStill:number,
+  timeStill:number,
+}
 
+/* Components for detailling during game */
+function InformationsGame({liveStill, timeStill}:InformationGameProps){
+  let index = 0;
+  const svg_size = "14%";
+  
   return <div className='div-menu-playing'>
     <h1>À vous de jouer ?!</h1>
     <div className='game-informations'>
@@ -143,26 +183,61 @@ function InformationsGame(liveStill:number, timeStill:number){
 }
 
 
+interface ConfigurationGameProps{
+  startGame:()=>void,
+  lastGameState:string,
+  setConfigLives:Dispatch<SetStateAction<number>>,
+  configLives:number,
+  setConfigGameTime:Dispatch<SetStateAction<number>>,
+  configGameTime:number,
+}
+
 /* Return a component witch is a div with fields stuffs 
 to configure game*/
-function ConfigurationGame(parentCallbackStartGame:(v:boolean)=>void,
-                      setLives:Dispatch<SetStateAction<number>>,
-                      configLives:number,
-                      setConfigLives:Dispatch<SetStateAction<number>>,
-                    )
+function ConfigurationGame({startGame,
+                      lastGameState,
+                      setConfigLives,
+                      configLives,
+                      setConfigGameTime,
+                      configGameTime}:ConfigurationGameProps)
 {
   const handleSubmitPlay = (e:SyntheticEvent) => {
     e.preventDefault();
-    setLives(configLives);
-    console.log("starting game");
-    parentCallbackStartGame(true);
+    // setLives(configLives);
+    startGame();
   };
+
+  const [doesUserAlreadyPlayed, setDoesUserAlreadyPlayed] = useState(false);
+  const [messageGameState, setMessageGameState] = useState("");
+
+
+  useEffect(() =>{
+    if (lastGameState != "none"){
+      setDoesUserAlreadyPlayed(true);
+      if (lastGameState == gameStatements.IS_WON){
+        setMessageGameState("Félicitation, la dernière partie est gagnée!");
+      } else {
+        setMessageGameState("Navré que la partie dernière n'est pas été gagnée");
+      }
+    };
+  },
+  [setDoesUserAlreadyPlayed, setMessageGameState]);
+
   return (
     <div className="div-menu">
-      <form onSubmit={handleSubmitPlay}>
+      <form className='space-evenly' onSubmit={startGame}>
         <h1>Configuration pour le jeu</h1>
-        <FieldGame elementName='life' isNum={true} setConfigLives={setConfigLives} configLives={configLives}/>
-        <button className='btn-play'>Jouer</button>
+        <FielConfigGame elementName='life' labelText={"Vie :"}
+              isNum={true} setConfigValue={setConfigLives} configValue={configLives}/>
+        
+        <FielConfigGame elementName='time' labelText={"Temps (secs) :"}
+                isNum={true} setConfigValue={setConfigGameTime} configValue={configGameTime}/>
+        
+        <div className='div-config-game-submit'>
+          <button className='btn-play'>Jouer</button>
+        </div>
+        {doesUserAlreadyPlayed == true ? <h3 className={lastGameState == gameStatements.IS_WON ? 'inform-won-game' : 'inform-lose-game'}>
+                                          {messageGameState}</h3> : null}
       </form>
     </div>
   )
@@ -181,35 +256,54 @@ interface InputGameProps{
   setLastInput:Dispatch<SetStateAction<number>>, //parent callback from 'app'
   setLives:Dispatch<SetStateAction<number>>,
   lives:number,
-  setIsPlaying:Dispatch<SetStateAction<boolean>>
+  setIsPlaying:Dispatch<SetStateAction<boolean>>,
+  setInputState:Dispatch<SetStateAction<string>>,
+  minimumValueNumInput:number,
+  maximumValueNumInput:number,
+  setLastGameState:Dispatch<SetStateAction<string>>,
 }
 
 // Input with button and label as one Component for the game
-function InputGame({numToFind,isPlaying, setLastInput, setLives, lives, setIsPlaying}:InputGameProps){
+function InputGame({numToFind,isPlaying, setLastInput, setLives, lives, setIsPlaying,
+          setInputState, minimumValueNumInput, maximumValueNumInput, setLastGameState}:InputGameProps)
+{
   const [valueUser, setValueUser] = useState(0);
 
   const handleSubmit = (e:SyntheticEvent) => {
     e.preventDefault();
       if (!isPlaying){
-      alert("Pas encore en jeu..!")
+      // alert("Pas encore en jeu..!")
       return;
     }
 
     setLastInput(valueUser);
-    if (lives > 0){
-      setLives(lives-boolToInt(valueUser!=numToFind));
-    } else {
-      setIsPlaying(false);
-    };
-    // e.target.value = "";
+    
+    const loseLife = boolToInt(valueUser!=numToFind);
+    if (loseLife == 1){
+      setLives(lives-1);
+      if (lives == 1){
+        setIsPlaying(false);
+        setLastGameState(gameStatements.IS_LOST);
+        console.log("perdu");
+      };
+    }
 
+
+    if (valueUser == numToFind){
+      setIsPlaying(false);
+      setLastGameState(gameStatements.IS_WON);
+    } else if (valueUser > numToFind){
+      setInputState(inputStates.IS_HIGHER);
+    } else if (valueUser < numToFind){
+      setInputState(inputStates.IS_LOWER);
+    }
   };
 
   return (
-    <div className="div-game-section">
+    <div id="answer" className="div-game-section">
         <form className='justify-left' onSubmit={handleSubmit}>
-          <label htmlFor="player-value">Votre réponse ?</label>
-          <input name="player-value" type="int" onChange={(e) => {
+          <label htmlFor="answer" aria-label="Votre réponse">Votre réponse ?</label>
+          <input name="player-value" aria-label='your answer' type="int" autoComplete='off' min={minimumValueNumInput} max={maximumValueNumInput} onChange={(e) => {
               setValueUser(Number(e.target.value))
             }
           } defaultValue="0"/>
@@ -222,13 +316,14 @@ function InputGame({numToFind,isPlaying, setLastInput, setLives, lives, setIsPla
 interface FieldGameProps {
   elementName:string,
   isNum: boolean,
-  setConfigLives:Dispatch<SetStateAction<number>>, // see parent callback 'app'
-  configLives:number
+  setConfigValue:Dispatch<SetStateAction<number>>, // see parent callback 'app'
+  configValue:number,
+  labelText:string
 }
 
 /* Return a component that is a set of elements for configuring game,
 Build div with label and input element*/
-function FieldGame({elementName, isNum, setConfigLives, configLives}:FieldGameProps){
+function FielConfigGame({elementName, isNum, setConfigValue, configValue, labelText}:FieldGameProps){
   let typeField = "normal";
   if (isNum){
     typeField = "numeric";
@@ -236,14 +331,14 @@ function FieldGame({elementName, isNum, setConfigLives, configLives}:FieldGamePr
 
   const inputCallback = (e:ChangeEvent<HTMLInputElement>) => { //set value for parent state 'app'
     console.log("e target value :", e.target.value);
-    setConfigLives(Number(e.target.value));
+    setConfigValue(Number(e.target.value));
   }
   
   return (
-    <div className={`div_${elementName}`}>
+    <div className={`div-config-game`}>
       <div>
-        <label className={`label_${elementName}`} htmlFor={`input-${elementName}`}>Vies :</label>
-        <input name={`input-${elementName}`} type={typeField} defaultValue={configLives} onChange={inputCallback}/>
+        <label className={`label_${elementName}`} htmlFor={`input-${elementName}`}>{labelText}</label>
+        <input name={`input-${elementName}`} type={typeField} defaultValue={configValue} onChange={inputCallback}/>
       </div>
     </div>
   )
